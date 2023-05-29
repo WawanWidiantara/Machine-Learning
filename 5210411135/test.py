8466fd6080170105e434909d95ff3ced29781303
WawanWidiantara <gdwidi13@gmail.com>
2023-05-29 12:34:24 +0700

    + pertemuan 11

--- 5210411135/test.py
+++ 5210411135/test.py
@@ -1,1 +1,11 @@
-print('test')
+import math
+
+data = [84,88,65,77,71,50,91,82,74,78,80,65]
+hasil = []
+for i in data:
+    h = ((i - 75.42)**2)
+    hasil.append(h)
+
+print((sum(hasil))/11)
+print((sum(data)))
+
